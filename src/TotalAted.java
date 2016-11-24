a008c684c742ca07465020aa63394b38beb8ba09
math-PC\math <shhong407@naver.com>
2016-11-24 20:29:49 +0900

    no message

---
+++ src/TotalAted.java
@@ -0,0 +1,32 @@
+public class TotalAted {
+	private static double tCal;
+	private static double tCarbo;
+	private static double tProt;
+	private static double tFat;
+	public static double gettCal() {
+		return tCal;
+	}
+	public static void settCal(double tCal) {
+		TotalAted.tCal = tCal;
+	}
+	public static double gettCarbo() {
+		return tCarbo;
+	}
+	public static void settCarbo(double tCarbo) {
+		TotalAted.tCarbo = tCarbo;
+	}
+	public static double gettProt() {
+		return tProt;
+	}
+	public static void settProt(double tProt) {
+		TotalAted.tProt = tProt;
+	}
+	public static double gettFat() {
+		return tFat;
+	}
+	public static void settFat(double tFat) {
+		TotalAted.tFat = tFat;
+	}
+
+
+}
